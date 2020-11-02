9ef95b126eae54b55c991cbd62bf2e79f2e59fac
Sara Wilcox <saraewilcox@gmail.com>
2020-11-02 16:25:52 +0000

    all done

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
-
-  add(item) {}
-
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  constructor(items, length) {
+  this.items = [];
+  this.length = this.items.length;
 }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
+      //return items;
+    }
+
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error("OutOfBounds")
+    }
+
+    return this.items[pos]
+  }
+  
+  max() {
+    if (!this.length) {
+      throw new Error("EmptySortedList")
+    }
+
+    return this.items [this.items.length -1];
+  }
+
+
+  min() {
+    if (!this.length) {
+      throw new Error("EmptySortedList")
+    }
+  
+    return Math.min(...this.items);
+  }
+
+
+  sum() {
+    if (!this.length) {
+      return 0;
+    }
+  
+    return this.items.reduce((value, sum) => value + sum);
+  }
+
+
+  avg() {
+    if (!this.length) {
+      throw new Error("EmptySortedList")
+    }
+  
+    return this.sum()/this.length;
+  }
+ }
 
 module.exports = SortedList;
+
